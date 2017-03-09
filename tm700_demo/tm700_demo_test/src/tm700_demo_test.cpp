5670f24644bf205b72cf29e72d17c4e0032c6f42
s880367 <s880367@gmail.com>
2017-03-10 02:03:32 +0800

    add get joint state function

--- tm700_demo/tm700_demo_test/src/tm700_demo_test.cpp
+++ tm700_demo/tm700_demo_test/src/tm700_demo_test.cpp
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *********************************************************************
- * 
+ *
  * Author: Yun-Hsuan Tsai
  */
 
@@ -44,6 +44,35 @@
 //#include "tm_msgs/SetIORequest.h"
 //#include "tm_msgs/SetIOResponse.h"
 
+//added by Yu-Hsien, 2017/3/9
+//get the current joint value of each joint
+bool get_current_joint_value(moveit::planning_interface::MoveGroup& group,
+                                moveit::planning_interface::MoveGroup::Plan& plan,
+															  const std::string& target_name,
+															  unsigned int max_try_times = 1
+															){
+			if(!ros::ok()) return false;
+			bool success = false;
+
+			for(unsigned int i= 0; i<max_try_times; i++){
+
+				group.getCurrentJointValues(target_name);
+        for(int i = 0; i<target_name.size(); i++){
+					printf("%f\n", target_name[i]);
+				}
+
+				if(group.move()){
+					success = true;
+					break;
+				}
+				else {
+					if(!ros::ok()) break;
+					sleep(1);
+				}
+			}
+			return success;
+}
+
 
 bool try_move_to_named_target(moveit::planning_interface::MoveGroup& group,
 			      moveit::planning_interface::MoveGroup::Plan& plan,
@@ -52,11 +81,11 @@ bool try_move_to_named_target(moveit::planning_interface::MoveGroup& group,
 			     ) {
   if(!ros::ok()) return false;
   bool success = false;
-  
+
   for(unsigned int i = 0; i < max_try_times; i++) {
-    
+
     group.setNamedTarget(target_name);
-    
+
     if(group.move()) {
       success = true;
       break;
@@ -76,11 +105,11 @@ bool try_move_to_joint_target(moveit::planning_interface::MoveGroup& group,
 			     ) {
   if(!ros::ok()) return false;
   bool success = false;
-  
+
   for(unsigned int i = 0; i < max_try_times; i++) {
-    
+
     group.setJointValueTarget(joint_target);
-    
+
     if(group.move()) {
       success = true;
       break;
@@ -102,17 +131,17 @@ int main(int argc, char **argv)
   io_srv.request.fun = 2;//tm_msgs::SetIORequest::FUN_SET_EE_DIGITAL_OUT;
   io_srv.request.ch = 0;
   io_srv.request.value = 0.0;
-  
+
   // start a background "spinner", so our node can process ROS messages
   //  - this lets us know when the move is completed
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  
+
   sleep(1);
-  
+
   // Setup
   // ^^^^^
-  // The :move_group_interface:`MoveGroup` class can be easily 
+  // The :move_group_interface:`MoveGroup` class can be easily
   // setup using just the name
   // of the group you would like to control and plan for.
   moveit::planning_interface::MoveGroup group("manipulator");
@@ -122,25 +151,25 @@ int main(int argc, char **argv)
   // (Optional) Create a publisher for visualizing plans in Rviz.
   //ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
   //moveit_msgs::DisplayTrajectory display_trajectory;
-  
+
   // Getting Basic Information
   // ^^^^^^^^^^^^^^^^^^^^^^^^^
   // We can print the name of the reference frame for this robot.
   ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
   // We can also print the name of the end-effector link for this group.
   ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
-  
+
   moveit::planning_interface::MoveGroup::Plan my_plan;
-  
+
   set_io_client.call(io_srv);
-  
+
   group.setPlanningTime(30.0);
-  
+
   try_move_to_named_target(group, my_plan, "home", 100);
-  
+
   std::vector<double> joint_target_1;
   std::vector<double> joint_target_2;
-  
+
   joint_target_1.assign(6, 0.0f);
   joint_target_1[0] = 0.0174533*( 30.0);
   joint_target_1[1] = 0.0174533*( 15.0);
@@ -148,16 +177,16 @@ int main(int argc, char **argv)
   joint_target_1[3] = 0.0174533*(-30.0);
   joint_target_1[4] = 0.0174533*( 90.0);
   joint_target_1[5] = 0.0174533*( 30.0);
-  
+
   joint_target_2.assign(6, 0.0f);
   joint_target_2[0] = 0.0174533*(-30.0);
   joint_target_2[1] = 0.0174533*(-15.0);
   joint_target_2[2] = 0.0174533*( 90.0);
   joint_target_2[3] = 0.0174533*(-75.0);
   joint_target_2[4] = 0.0174533*(120.0);
-  
+
   int step = 0;
-  
+
   while (ros::ok()) {
     switch (step) {
       case 0: //gripper off
@@ -169,12 +198,12 @@ int main(int argc, char **argv)
 	else
 	  ROS_WARN("Failed to call service set_io");
 	break;
-	
+
       case 1: //move to ready
 	ROS_INFO("move...");
 	try_move_to_named_target(group, my_plan, "ready", 100);
 	break;
-	
+
       case 2: //light on
 	io_srv.request.fun = 2;
 	io_srv.request.ch = 3;
@@ -184,26 +213,26 @@ int main(int argc, char **argv)
 	else
 	  ROS_WARN("Failed to call service set_io");
 	break;
-      
+
       case 3: //wait 3 sec
 	sleep(3);
 	break;
-	
+
       case 4: //light off
 	io_srv.request.fun = 2;
 	io_srv.request.ch = 3;
 	io_srv.request.value = 0.0;
 	if (set_io_client.call(io_srv))
 	  ROS_INFO("Set IO success");
-	else 
+	else
 	  ROS_WARN("Failed to call service set_io");
 	break;
-	
+
       case 5: //move 1
 	ROS_INFO("move...");
 	try_move_to_joint_target(group, my_plan, joint_target_1, 100);
 	break;
-	
+
       case 6: //gripper on
 	io_srv.request.fun = 2;
 	io_srv.request.ch = 0;
@@ -213,11 +242,11 @@ int main(int argc, char **argv)
 	else
 	  ROS_WARN("Failed to call service set_io");
 	break;
-	
+
       case 7: //wait 1 sec
 	sleep(1);
 	break;
-	
+
       case 8: //move 2...
 	ROS_INFO("move...");
 	try_move_to_joint_target(group, my_plan, joint_target_2, 100);
